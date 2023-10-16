bac51b6a80fa06d5feb92f32b76c23ee3a40ff14
EvertonLDS815 <evertonlino1234@gmail.com>
2023-10-15 21:47:45 -0300

    feat: add socketio on code and http

--- index.js
+++ index.js
@@ -1,32 +1,40 @@
-require("dotenv").config();
-const connetToDb = require("./database/db");
+require('dotenv').config();
+const connetToDb = require('./database/db');
+const http = require('http');
+const { Server } = require('socket.io');
+const express = require('express');
 
-const express = require("express");
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
 const port = process.env.PORT || 300;
-const cors = require("cors");
+const cors = require('cors');
 
 app.use(express.json());
 app.use(cors());
-const Product = require("./model/product");
+const Product = require('./model/product');
 
 connetToDb();
 
-app.get("/", async (req, res) => {
+app.get('/', async (req, res) => {
   const ports = await Product.find();
   res.json(ports);
 });
 
-app.post("/ins", async (req, res) => {
+app.post('/ins', async (req, res) => {
   const newPort = req.body;
-  await Product.create(newPort);
+  const product = await Product.create(newPort);
 
-  res.status(201).json(newPort);
+  const productDetails = await product.populate();
+  io.emit('orders@new', productDetails);
+
+  res.status(201).json(product);
 });
-app.put("/ins/:id", async (req, res) => {
+app.put('/ins/:id', async (req, res) => {
   const { id } = req.params;
   const { name, image, description, quantity, code } = req.body;
-  await Product.findByIdAndUpdate(id, {
+  const product = await Product.findByIdAndUpdate(id, {
     name,
     image,
     description,
@@ -36,11 +44,11 @@ app.put("/ins/:id", async (req, res) => {
 
   res.sendStatus(204);
 });
-app.delete("/ins/:id", async (req, res) => {
+app.delete('/ins/:id', async (req, res) => {
   const { id } = req.params;
-  await Product.findByIdAndDelete(id);
+  const product = await Product.findByIdAndDelete(id);
 
   res.sendStatus(204);
 });
 
-app.listen(port, () => console.log(`🚀 Meu site http://localhost:${port}`));
+server.listen(port, () => console.log(`🚀 Meu site http://localhost:${port}`));
